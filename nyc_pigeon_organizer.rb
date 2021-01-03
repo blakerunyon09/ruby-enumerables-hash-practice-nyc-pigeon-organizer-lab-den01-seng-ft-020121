742053f9c6f207dd33e7fd0cf1b468400f87d6f8
Blake Runyon <blakerunyon09@gmail.com>
2021-01-03 06:08:40 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,38 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.map { |top_category, sub_category|
+    sub_category.map { |sub_category, names|
+      names.each { |name|
+        if pigeon_list[name] === nil
+          pigeon_list[name] = {}
+        end
+        if pigeon_list[name][top_category] === nil
+          pigeon_list[name][top_category] = []
+        end
+        pigeon_list[name][top_category].push(sub_category.to_s)
+      }
+    }
+  }
+  pigeon_list
 end
+
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
+nyc_pigeon_organizer(pigeon_data)
