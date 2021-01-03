e12822e97ddbaf1f10b46dcf5a8f9b5829e22c97
Blake Runyon <blakerunyon09@gmail.com>
2021-01-03 05:13:01 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,36 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  counter = 0
+  #creates hash of names without values
+  data[:color].values.map { |e|
+    e.map { |e|
+      new_hash[e] = []
+     }
+  }
+  #adds values to hashes
+  data.values.map { |h|
+    counter += 1
+  }
+  p counter
 end
+
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
+nyc_pigeon_organizer(pigeon_data)
